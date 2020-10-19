6e986e396791b57a7913367b151b3d15ffe5521a
Abhishek Verma <verma.abhishek0144@gmail.com>
2020-10-20 03:15:52 +0530

    Updates

---
+++ precache-manifest.35abd10e6dad79805680f312717a211a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa5212adcc0836a194c8dec896798f3",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "55c507090f81fc81ad86",
+    "url": "/BookStore/static/css/2.225f2bff.chunk.css"
+  },
+  {
+    "revision": "d2d4a17903c5b1c6acb9",
+    "url": "/BookStore/static/css/main.cd946d77.chunk.css"
+  },
+  {
+    "revision": "55c507090f81fc81ad86",
+    "url": "/BookStore/static/js/2.49cbe15e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/BookStore/static/js/2.49cbe15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d4a17903c5b1c6acb9",
+    "url": "/BookStore/static/js/main.3edeb8d2.chunk.js"
+  },
+  {
+    "revision": "1337ca815507dea61d66",
+    "url": "/BookStore/static/js/runtime-main.0bbabbdd.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/BookStore/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/BookStore/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/BookStore/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/BookStore/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/BookStore/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/BookStore/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/BookStore/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/BookStore/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/BookStore/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/BookStore/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/BookStore/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/BookStore/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/BookStore/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/BookStore/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/BookStore/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "6c6ed370fba3bcf6192697dcf82c2be8",
+    "url": "/BookStore/static/media/logo.6c6ed370.JPG"
+  }
+]);
